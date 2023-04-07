2590860c47ff9a8830ad205d520d918ad2155367
salty <salty@salty.dk>
2023-04-07 13:50:07 +0200

    initial implementation

---
+++ main.py
@@ -0,0 +1,177 @@
+import os
+import re
+import time
+import requests
+import CloudFlare
+import tldextract
+
+# Your Cloudflare global API key and email
+CLOUDFLARE_API_KEY = os.environ.get('CLOUDFLARE_API_KEY')
+CLOUDFLARE_EMAIL = os.environ.get('CLOUDFLARE_EMAIL')
+CLOUDFLARE_PROXY_DEFAULT = os.environ.get('CLOUDFLARE_PROXY_DEFAULT', False)
+
+# Traefik API URL
+TRAEFIK_API_URL = os.environ.get('TRAEFIK_API_URL')
+
+# IP Version: '4' for IPv4 only, '6' for IPv6 only, or "both" for both IPv4 and IPv6
+IP_VERSION = os.environ.get('IP_VERSION', 'both')
+
+# Initialize Cloudflare client
+cf = CloudFlare.CloudFlare(email=CLOUDFLARE_EMAIL, token=CLOUDFLARE_API_KEY)
+
+
+def get_wan_ip(ip_version):
+    """Returns WAN IP for the given IP version"""
+    response = requests.get(f"https://ipify{'6' if ip_version == 6 else ''}.saltbox.dev?format=json")
+    return response.json()["ip"]
+
+
+def get_wan_ips():
+    """Returns WAN IPs for both IPv4 and IPv6 if IP_VERSION is set to "both", else returns IP for the set version"""
+    if IP_VERSION == "both":
+        return {4: get_wan_ip(4), 6: get_wan_ip(6)}
+    elif IP_VERSION == '4':
+        return {4: get_wan_ip(4)}
+    elif IP_VERSION == '6':
+        return {6: get_wan_ip(6)}
+    else:
+        raise ValueError("Invalid IP version")
+
+
+def get_traefik_routers():
+    """Returns Traefik http routers from the given API URL"""
+    response = requests.get(f"{TRAEFIK_API_URL}/http/routers")
+    return response.json()
+
+
+def get_cloudflare_zones():
+    """Returns all Cloudflare zones"""
+    return cf.zones.get()
+
+
+def get_zone_id(domain):
+    """Returns zone ID for the given domain"""
+    zones = get_cloudflare_zones()
+    return next((zone['id'] for zone in zones if zone['name'] == domain), None)
+
+
+def update_cloudflare_records(routers, wan_ips):
+    """Updates Cloudflare DNS records for the given http routers and WAN IPs"""
+    processed_zones = {}
+    processed_hosts = set()  # Keep track of processed rule hosts
+
+    for router in routers.values():
+        rule = router.get('rule', '')
+        host_match = re.search(r"Host\(`(.*?)`\)", rule)
+        if not host_match:
+            continue
+
+        host = host_match[1]
+        extracted_domain = tldextract.extract(host)
+        root_domain = ".".join(extracted_domain[1:])
+
+        if not extracted_domain.suffix:
+            print(f"Skipping {host} as it's not a valid domain")
+            continue
+
+        if root_domain in processed_zones:
+            zone_id, dns_records = processed_zones[root_domain]
+        else:
+            zone_id = get_zone_id(root_domain)
+            if not zone_id:
+                print(f"Zone for domain {root_domain} not found")
+                continue
+
+            dns_records = []
+            page_num = 1
+            while True:
+                page_records = cf.zones.dns_records.get(zone_id, params={'page': page_num})
+                if not page_records:
+                    break
+                dns_records.extend(page_records)
+                page_num += 1
+
+            processed_zones[root_domain] = (zone_id, dns_records)
+
+        existing_a_records = {record['name']: record for record in dns_records if record['type'] == 'A'}
+        existing_aaaa_records = {record['name']: record for record in dns_records if record['type'] == 'AAAA'}
+
+        # Check if the rule host has already been processed
+        if host in processed_hosts:
+            continue
+        processed_hosts.add(host)
+
+        for ip_version, ip in wan_ips.items():
+            if ip_version == 4:
+                record_type = 'A'
+                existing_records = existing_a_records
+            elif ip_version == 6:
+                record_type = 'AAAA'
+                existing_records = existing_aaaa_records
+            else:
+                print(f"Invalid IP version: {ip_version}")
+                continue
+
+            if host in existing_records:
+                record = existing_records[host]
+                if record['content'] != ip:
+                    print(f"Updating {record_type} record for {host}")
+                    cf.zones.dns_records.put(zone_id, record['id'], data={
+                        'type': record_type,
+                        'name': host,
+                        'content': ip,
+                        'proxied': record['proxied']
+                    })
+            else:
+                print(f"Adding {record_type} record for {host}")
+                cf.zones.dns_records.post(zone_id, data={
+                    'type': record_type,
+                    'name': host,
+                    'content': ip,
+                    'proxied': CLOUDFLARE_PROXY_DEFAULT
+                })
+
+
+def main():
+    """Main loop"""
+    # Check if all required environment variables are set
+    if not all([CLOUDFLARE_API_KEY, CLOUDFLARE_EMAIL, TRAEFIK_API_URL, IP_VERSION]):
+        print(
+            "Please set the required environment variables: CLOUDFLARE_API_KEY, CLOUDFLARE_EMAIL, TRAEFIK_API_URL, "
+            "IP_VERSION")
+        return
+
+    # Validate Cloudflare global API key
+    try:
+        get_cloudflare_zones()
+    except Exception:
+        print("Invalid Cloudflare global API key")
+        return
+
+    # Initialize variables
+    last_wan_ips = {}
+    routers = {router['name']: router for router in get_traefik_routers()}
+
+    while True:
+        try:
+            new_routers = {router['name']: router for router in get_traefik_routers()}
+            new_wan_ips = get_wan_ips()
+            if new_wan_ips != last_wan_ips:
+                print(f"WAN IPs changed to {new_wan_ips}")
+                last_wan_ips = new_wan_ips
+                update_cloudflare_records(new_routers, new_wan_ips)
+
+            added_routers = {k: v for k, v in new_routers.items() if k not in routers}
+            routers = new_routers
+            if added_routers:
+                update_cloudflare_records(added_routers, new_wan_ips)
+            else:
+                print("No new routers found")
+        except Exception as e:
+            print(f"Error: {e}")
+
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
